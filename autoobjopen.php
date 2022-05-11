e18626ad148201d098f831cc62a9842e0a8f2675
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-11 02:17:46 +0000

    Changed paradigm

--- autoobjopen.php
+++ autoobjopen.php
@@ -3,6 +3,18 @@ $objRating = file_get_contents($obj.'/rating');
 $objMode = file_get_contents($obj.'/mode');
 $objCoord = file_get_contents($obj.'/coord');
 $objCoordDiv = explode(';', $objCoord);
-$objX = $objCoordDiv[0] ?? 0;
-$objY = $objCoordDiv[1] ?? 0;
-$objZ = $objCoordDiv[2] ?? 0;
+if (is_numeric($objX)) {
+    $objX = $objCoordDiv[0];
+} else {
+    $objX = 0;
+}
+if (is_numeric($objY)) {
+    $objY = $objCoordDiv[1];
+} else {
+    $objY = 0;
+}
+if (is_numeric($objZ)) {
+    $objZ = $objCoordDiv[2];
+} else {
+    $objZ = 0;
+}
--- autosubopen.php
+++ autosubopen.php
@@ -3,6 +3,18 @@ $subRating = file_get_contents($sub.'/rating');
 $subMode = file_get_contents($sub.'/mode');
 $subCoord = file_get_contents($sub.'/coord');
 $subCoordDiv = explode(';', $subCoord);
-$subX = $subCoordDiv[0] ?? 0;
-$subY = $subCoordDiv[1] ?? 0;
-$subZ = $subCoordDiv[2] ?? 0;
+if (is_numeric($subX)) {
+    $subX = $subCoordDiv[0];
+} else {
+    $subX = 0;
+}
+if (is_numeric($subY)) {
+    $subY = $subCoordDiv[1];
+} else {
+    $subY = 0;
+}
+if (is_numeric($subZ)) {
+    $subZ = $subCoordDiv[2];
+} else {
+    $subZ = 0;
+}
