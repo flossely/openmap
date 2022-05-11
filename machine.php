3dc5f508a628cec89c45e1dccf0ebdec386271aa
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-11 02:44:08 +0000

    Changed paradigm

--- machine.php
+++ machine.php
@@ -7,15 +7,15 @@ if ($sub == $obj) {
             if (($subMode == 0 && $objMode == 0) || ($subMode > 0 && $objMode < 0) || ($subMode < 0 && $objMode > 0)) {
                 $objRating = $objRating - $subForce;
                 $subRating = $subRating + $subForce;
-                echo $sub.' ('.$subRating.') - '.$obj.' ('.$objRating.')<br>';
+                echo $sub.' ('.$subRating.') harmed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
             } elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0)) {
                 $objRating = $objRating + $subForce;
                 $subRating = $subRating - $subForce;
-                echo $sub.' ('.$subRating.') + '.$obj.' ('.$objRating.')<br>';
+                echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
             } elseif (($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
                 $objRating = $objRating + $subForce;
                 $subRating = $subRating - $subForce;
-                echo $sub.' ('.$subRating.') + '.$obj.' ('.$objRating.')<br>';
+                echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
             }
         } elseif ($objRating < 0) {
             echo $sub.' to '.$obj.': good riddance<br>';
