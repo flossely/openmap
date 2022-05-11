20b3614ad8e6c7ec6da3214fbf17ef921bcf135b
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-11 03:20:13 +0000

    Changed paradigm

--- discover.php
+++ discover.php
@@ -1,26 +1,22 @@
 <?php
-if ($subRating >= 0) {
-    if ($objRating >= 0) {
-        if (($subMode == 0 && $objMode == 0) || ($subMode > 0 && $objMode < 0) || ($subMode < 0 && $objMode > 0)) {
-            $objRating = $objRating - $subForce;
-            $subRating = $subRating + $subForce;
-            echo $sub.' ('.$subRating.') harmed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        } elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0)) {
-            $objRating = $objRating + $subForce;
-            $subRating = $subRating - $subForce;
-            echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        } elseif (($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
-            $objRating = $objRating + $subForce;
-            $subRating = $subRating - $subForce;
-            echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        }
-    } elseif ($objRating < 0) {
-        echo $sub.' to '.$obj.': good riddance<br>';
-    }
-} elseif ($subRating < 0) {
-    if ($objRating >= 0) {
-        echo $obj.' to '.$sub.': good riddance<br>';
-    } elseif ($objRating < 0) {
-        echo $sub.' ('.$subRating.') is dead<br>';
-    }
+if ($subX > $objX) {
+    $distX = $subX - $objX;
+} elseif ($subX < $objX) {
+    $distX = $objX - $subX;
+} elseif ($subX == $objX) {
+    $distX = 0;
+}
+if ($subY > $objY) {
+    $distY = $subY - $objY;
+} elseif ($subY < $objY) {
+    $distY = $objY - $subY;
+} elseif ($subY == $objY) {
+    $distY = 0;
+}
+if ($subZ > $objZ) {
+    $distZ = $subZ - $objZ;
+} elseif ($subZ < $objZ) {
+    $distZ = $objZ - $subZ;
+} elseif ($subZ == $objZ) {
+    $distZ = 0;
 }
--- machine.php
+++ machine.php
@@ -1,31 +1,19 @@
 <?php
 if ($sub == $obj) {
-    echo $sub.' ('.$subRating.') is thinking...<br>';
+    if ($subRating >= 0) {
+        echo $sub.' ('.$subRating.') is thinking...<br>';
+    } elseif ($subRating < 0) {
+        echo $sub.' ('.$subRating.') is dead<br>';
+    }
 } elseif ($sub != $obj) {
-    if ($subX > $objX) {
-        $distX = $subX - $objX;
-    } elseif ($subX < $objX) {
-        $distX = $objX - $subX;
-    } elseif ($subX == $objX) {
-        $distX = 0;
-    }
-    if ($subY > $objY) {
-        $distY = $subY - $objY;
-    } elseif ($subY < $objY) {
-        $distY = $objY - $subY;
-    } elseif ($subY == $objY) {
-        $distY = 0;
-    }
-    if ($subZ > $objZ) {
-        $distZ = $subZ - $objZ;
-    } elseif ($subZ < $objZ) {
-        $distZ = $objZ - $subZ;
-    } elseif ($subZ == $objZ) {
-        $distZ = 0;
-    }
-    if (($distX <= $subRating) && ($distY <= $subRating) && ($distZ <= $subRating)) {
+    if ($subRating >= 0) {
         include 'discover.php';
-    } else {
-        echo $sub.': '.$obj.' not reachable<br>';
+        if (($distX <= $subRating) && ($distY <= $subRating) && ($distZ <= $subRating)) {
+            include 'discovered.php';
+        } else {
+            echo $sub.': '.$obj.' not reachable<br>';
+        }
+    } elseif ($subRating < 0) {
+        echo $sub.' ('.$subRating.') is dead<br>';
     }
 }
