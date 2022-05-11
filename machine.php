214858d70c138f53ed0025068d1a5fe0eebfe4bc
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-11 03:35:29 +0000

    Changed paradigm

--- machine.php
+++ machine.php
@@ -1,7 +1,18 @@
 <?php
 if ($sub == $obj) {
     if ($subRating >= 0) {
-        echo $sub.' ('.$subRating.') is thinking...<br>';
+        $subAct = rand(0,1);
+        if ($subAct == 0) {
+            echo $sub.' ('.$subRating.') is thinking...<br>';
+        } elseif ($subAct == 1) {
+            $arc = ['d','r','u','l'];
+            $arcRand = rand(0,count($arc)-1);
+            if (file_exists($sub.'/foot.'.$arc[$arcRand].'.png')) {
+                echo $sub." has opened a <a href='".$sub."/foot.".$arc[$arcRand].".png'>brothel</a>";
+            } else {
+                echo $sub.' ('.$subRating.') is thinking...<br>';
+            }
+        }
     } elseif ($subRating < 0) {
         echo $sub.' ('.$subRating.') is dead<br>';
     }
