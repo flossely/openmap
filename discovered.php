29dacac34952b541595a815867e5539f230cac63
Karl August Nestor <jesuscaesar@outlook.com>
2022-05-11 04:05:50 +0000

    Changed paradigm

--- discovered.php
+++ discovered.php
@@ -1,26 +1,40 @@
 <?php
-if ($subRating >= 0) {
-    if ($objRating >= 0) {
-        if (($subMode == 0 && $objMode == 0) || ($subMode > 0 && $objMode < 0) || ($subMode < 0 && $objMode > 0)) {
-            $objRating = $objRating - $subForce;
-            $subRating = $subRating + $subForce;
-            echo $sub.' ('.$subRating.') harmed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        } elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0)) {
-            $objRating = $objRating + $subForce;
-            $subRating = $subRating - $subForce;
-            echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        } elseif (($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
-            $objRating = $objRating + $subForce;
-            $subRating = $subRating - $subForce;
-            echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
-        }
-    } elseif ($objRating < 0) {
-        echo $sub.' to '.$obj.': good riddance<br>';
+if ($objRating >= 0) {
+    if (($subMode == 0 && $objMode == 0) || ($subMode > 0 && $objMode < 0) || ($subMode < 0 && $objMode > 0)) {
+        $objRating = $objRating - $subForce;
+        $subRating = $subRating + $subForce;
+        echo $sub.' ('.$subRating.') harmed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
+    } elseif (($subMode > 0 && $objMode > 0) || ($subMode < 0 && $objMode < 0)) {
+        $objRating = $objRating + $subForce;
+        $subRating = $subRating - $subForce;
+        echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
+    } elseif (($subMode > 0 && $objMode == 0) || ($subMode < 0 && $objMode == 0) || ($subMode == 0 && $objMode > 0) || ($subMode == 0 && $objMode < 0)) {
+        $objRating = $objRating + $subForce;
+        $subRating = $subRating - $subForce;
+        echo $sub.' ('.$subRating.') healed ('.$subForce.') '.$obj.' ('.$objRating.')<br>';
     }
-} elseif ($subRating < 0) {
-    if ($objRating >= 0) {
-        echo $obj.' to '.$sub.': good riddance<br>';
-    } elseif ($objRating < 0) {
-        echo $sub.' ('.$subRating.') is dead<br>';
-    }
+} elseif ($objRating < 0) {
+    $say =
+    [
+        '-1' =>
+        [
+            'You are cancer',
+            'Be damned',
+            'How good you died',
+        ],
+        '0' =>
+        [
+            'Burn in hell',
+            'It serves you right',
+            'Much better off without you',
+        ],
+        '1' =>
+        [
+            'Good riddance',
+            'Hopefully you get what you deserve',
+            'You are human scum',
+        ],
+    ];
+    $sayRand = rand(0,2);
+    echo $sub.' to '.$obj.': '.$say[$subMode][$sayRand].'<br>';
 }
